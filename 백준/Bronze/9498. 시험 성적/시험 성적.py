06649321faf9b450f91faa126ca0a5cba4893d59
kth4778 <cnblue123429@gmail.com>
2024-02-20 09:44:55 +0900

    [Bronze V] Title: 시험 성적, Time: 44 ms, Memory: 31120 KB

---
+++ 백준/Bronze/9498. 시험 성적/시험 성적.py
@@ -0,0 +1,13 @@
+a=int(input())
+if 90<=a<=100:
+    print('A')
+elif 80<=a<=89:
+    print('B')
+elif 70<=a<=79:
+    print('C')
+elif 60<=a<=69:
+    print('D')
+elif 0<=a<=59:
+    print('F')
+else:
+    print('올바른 수를 입력해 주세요.')

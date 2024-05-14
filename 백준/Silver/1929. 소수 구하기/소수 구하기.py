16feaa1519bbb392e35868dfdb70d34cba4a6ce7
kth4778 <cnblue123429@gmail.com>
2024-05-14 20:34:44 +0900

    [Silver III] Title: 소수 구하기, Time: 3204 ms, Memory: 31120 KB

---
+++ 백준/Silver/1929. 소수 구하기/소수 구하기.py
@@ -0,0 +1,12 @@
+def sosu(a):
+    if a<2:
+        return False
+    for i in range(2,int(a**0.5)+1):
+        if a%i==0:
+            return False
+    return True
+q,w=map(int,input().split())
+for i in range(q,w+1):
+    if sosu(i):
+        print(i)
+

a23bc04bdaf9af5e3058bd328c44e7d535fd157f
kth4778 <cnblue123429@gmail.com>
2025-04-03 14:10:13 +0900

    [Gold IV] Title: 동전 1, Time: 184 ms, Memory: 33432 KB

---
+++ 백준/Gold/2293. 동전 1/동전 1.py
@@ -0,0 +1,13 @@
+import sys
+input = sys.stdin.readline
+
+n,k = map(int,input().split())
+numbers = [int(input()) for _ in range(n)]
+dp = [0 for _ in range(k + 1)]
+dp[0] = 1
+
+for i in numbers:
+    for j in range(i, k + 1):
+        dp[j] += dp[j - i]
+
+print(dp[k])

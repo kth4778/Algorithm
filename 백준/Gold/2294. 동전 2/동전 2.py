df857d6e97e459f4d2244609461132f84a7da724
kth4778 <cnblue123429@gmail.com>
2025-04-06 15:49:29 +0900

    [Gold V] Title: 동전 2, Time: 300 ms, Memory: 33432 KB

---
+++ 백준/Gold/2294. 동전 2/동전 2.py
@@ -0,0 +1,13 @@
+n,k = map(int,input().split())
+coins = [int(input()) for _ in range(n)]
+dp = [float("INF") for _ in range(k + 1)]
+dp[0] = 0
+
+for coin in coins:
+    for i in range(coin, k+1):
+        dp[i] = min(dp[i], dp[i - coin] + 1)
+
+if dp[k] == float("INF"):
+    print(-1)
+else:
+    print(dp[k])

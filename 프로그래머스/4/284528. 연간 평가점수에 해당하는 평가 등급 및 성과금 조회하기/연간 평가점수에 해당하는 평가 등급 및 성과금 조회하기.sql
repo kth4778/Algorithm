6661433f9b6120d0c671798d6b4b685c18b0e860
kth4778 <cnblue123429@gmail.com>
2026-03-06 16:30:20 +0900

    [level 4] Title: 연간 평가점수에 해당하는 평가 등급 및 성과금 조회하기, Time: , Memory: undefined -BaekjoonHub

--- 프로그래머스/4/284528. 연간 평가점수에 해당하는 평가 등급 및 성과금 조회하기/연간 평가점수에 해당하는 평가 등급 및 성과금 조회하기.sql
+++ 프로그래머스/4/284528. 연간 평가점수에 해당하는 평가 등급 및 성과금 조회하기/연간 평가점수에 해당하는 평가 등급 및 성과금 조회하기.sql
@@ -1,24 +1,22 @@
 SELECT 
-    E.EMP_NO, E.EMP_NAME, G.GRADE, G.PERCENT * E.SAL AS BONUS
-FROM 
-    HR_EMPLOYEES E
+    E.EMP_NO, 
+    E.EMP_NAME, 
+    G.GRADE,
+    CASE
+        WHEN G.GRADE = 'S' THEN E.SAL * 0.2 
+        WHEN G.GRADE = 'A' THEN E.SAL * 0.15
+        WHEN G.GRADE = 'B' THEN E.SAL * 0.1
+        ELSE 0 END AS BONUS 
+FROM HR_EMPLOYEES E
 JOIN (
-    SELECT
+    SELECT 
+        EMP_NO,
         CASE
             WHEN AVG(SCORE) >= 96 THEN 'S'
             WHEN AVG(SCORE) >= 90 THEN 'A'
             WHEN AVG(SCORE) >= 80 THEN 'B'
-            ELSE 'C'
-            END AS GRADE,
-        CASE
-            WHEN AVG(SCORE) >= 96 THEN 0.2
-            WHEN AVG(SCORE) >= 90 THEN 0.15
-            WHEN AVG(SCORE) >= 80 THEN 0.1
-            ELSE 0
-            END AS PERCENT,
-        EMP_NO
+            ELSE 'C' END AS GRADE
     FROM HR_GRADE
     GROUP BY EMP_NO
 ) G ON E.EMP_NO = G.EMP_NO
-ORDER BY
-    EMP_NO ASC;
+ORDER BY EMP_NO ASC;

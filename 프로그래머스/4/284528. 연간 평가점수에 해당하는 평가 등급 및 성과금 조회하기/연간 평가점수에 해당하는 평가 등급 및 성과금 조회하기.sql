4bb983d9b2da5f63ef062054a788e57762401e17
Kim TaeHyeon <cnblue123429@gmail.com>
2026-04-04 12:20:14 +0900

    [level 4] Title: 연간 평가점수에 해당하는 평가 등급 및 성과금 조회하기, Time: , Memory: undefined -BaekjoonHub

--- 프로그래머스/4/284528. 연간 평가점수에 해당하는 평가 등급 및 성과금 조회하기/연간 평가점수에 해당하는 평가 등급 및 성과금 조회하기.sql
+++ 프로그래머스/4/284528. 연간 평가점수에 해당하는 평가 등급 및 성과금 조회하기/연간 평가점수에 해당하는 평가 등급 및 성과금 조회하기.sql
@@ -1,16 +1,12 @@
-SELECT 
-    E.EMP_NO, 
-    E.EMP_NAME, 
-    G.GRADE,
-    CASE
-        WHEN G.GRADE = 'S' THEN E.SAL * 0.2 
-        WHEN G.GRADE = 'A' THEN E.SAL * 0.15
-        WHEN G.GRADE = 'B' THEN E.SAL * 0.1
-        ELSE 0 END AS BONUS 
+SELECT E.EMP_NO, E.EMP_NAME, G.GRADE, 
+    CASE 
+        WHEN G.GRADE = 'S' THEN SAL * 0.2
+        WHEN G.GRADE = 'A' THEN SAL * 0.15
+        WHEN G.GRADE = 'B' THEN SAL * 0.1
+        ELSE 0 END AS BONUS
 FROM HR_EMPLOYEES E
 JOIN (
-    SELECT 
-        EMP_NO,
+    SELECT EMP_NO,
         CASE
             WHEN AVG(SCORE) >= 96 THEN 'S'
             WHEN AVG(SCORE) >= 90 THEN 'A'
